582a9068f53ad48967b67e220bd5fd2027f9a539
hina2113 <110330118+hinaplugin@users.noreply.github.com>
2024-07-23 20:02:17 +0900

    Update roleset.js
    エラーハンドリングの追加

--- commands/roleset.js
+++ commands/roleset.js
@@ -14,44 +14,50 @@ module.exports = {
         const { commandName } = interaction;
 
         if (commandName === "roleset") {
-            const mainGuild = await client.guilds.cache.get(process.env.MAIN_GUILD_ID);
-            if (!mainGuild) {
-                await interaction.reply(`メインサーバーを取得できませんでした．`);
-                return;
-            }
-    
-            const mainRole = await mainGuild.roles.fetch(process.env.MAIN_ROLE_ID);
-            if (!mainRole) {
-                await interaction.reply(`メインロールを取得できませんでした．`);
-                return;
-            }
-    
-            const targetGuild = await client.guilds.cache.get(process.env.TARGET_GUILD_ID);
-            if (!targetGuild) {
-                await interaction.reply(`ターゲットサーバーを取得できませんでした．`);
-                return;
-            }
-    
-            const targetRole = await targetGuild.roles.fetch(process.env.TARGET_ROLE_ID);
-            if (!targetRole) {
-                await interaction.reply(`ターゲットロールを取得できませんでした．`);
-            }
-    
-            const mainMembers = await mainGuild.members.fetch();
-            const targetMembers = await targetGuild.members.fetch();
-    
-            await interaction.deferReply();
-    
-            await targetMembers.forEach(async (member) => {
-                const mainMember = await mainMembers.get(member.id);
-                if (mainMember) {
-                    if (mainMember.roles.cache.has(process.env.MAIN_ROLE_ID)) {
-                        await member.roles.add(targetRole);
-                    }
+            try{
+                const mainGuild = await client.guilds.cache.get(process.env.MAIN_GUILD_ID);
+                if (!mainGuild) {
+                    await interaction.reply(`メインサーバーを取得できませんでした．`);
+                    return;
                 }
-            });
-    
-            await interaction.editReply(`ロールの付与処理が完了しました．`);
+        
+                const mainRole = await mainGuild.roles.fetch(process.env.MAIN_ROLE_ID);
+                if (!mainRole) {
+                    await interaction.reply(`メインロールを取得できませんでした．`);
+                    return;
+                }
+        
+                const targetGuild = await client.guilds.cache.get(process.env.TARGET_GUILD_ID);
+                if (!targetGuild) {
+                    await interaction.reply(`ターゲットサーバーを取得できませんでした．`);
+                    return;
+                }
+        
+                const targetRole = await targetGuild.roles.fetch(process.env.TARGET_ROLE_ID);
+                if (!targetRole) {
+                    await interaction.reply(`ターゲットロールを取得できませんでした．`);
+                }
+        
+                const mainMembers = await mainGuild.members.fetch();
+                const targetMembers = await targetGuild.members.fetch();
+        
+                await interaction.deferReply();
+        
+                await targetMembers.forEach(async (member) => {
+                    if (!member.user.bot && !member.roles.cache.has(targetRole)) {
+                        const mainMember = await mainMembers.get(member.id);
+                        if (mainMember) {
+                            if (mainMember.roles.cache.has(process.env.MAIN_ROLE_ID)) {
+                                await member.roles.add(targetRole);
+                            }
+                        }
+                    }
+                });
+
+                await interaction.editReply(`ロールの付与処理が完了しました．`);
+            }catch (error){
+                console.log(error);
+            }
         }
     },
 };
