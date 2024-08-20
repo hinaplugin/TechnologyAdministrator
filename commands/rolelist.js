cf85126fbb3b397c6a0a3c19f0d3c021c3a5f249
hina2113 <110330118+hinaplugin@users.noreply.github.com>
2024-08-21 00:50:01 +0900

    Add files via upload

---
+++ commands/rolelist.js
@@ -0,0 +1,43 @@
+/**
+ * モジュールの読み込み
+ */
+const { SlashCommandBuilder } = require('discord.js');
+const fs = require('fs');
+const toml = require('@iarna/toml');
+const path = require('path');
+
+/**
+ * tomlファイルのパス
+ */
+const filePath = path.resolve(__dirname, "../../config.toml");
+
+/**
+ * モジュール作成
+ */
+module.exports = {
+    data: new SlashCommandBuilder()
+                .setName('rolelist')
+                .setDescription('ロール所有者のリスト取得'),
+    execute: async function(interaction) {
+        await interaction.deferReply();
+
+        const tomlContent = fs.readFileSync(filePath, 'utf-8');
+
+        const config = toml.parse(tomlContent);
+
+        if (config.panelList && Array.isArray(config.panelList)) {
+            for (let index = 0; index < config.panelList.length; index++) {
+                const roles = config.panelList[index];
+                if (roles) {
+                    const role = await interaction.guild.roles.fetch(roles);
+                    if (role) {
+                        const name = role.name;
+                        const members = role.members;
+                        console.log(name);
+                        console.log(members);
+                    }
+                }
+            }
+        }
+    }
+}
