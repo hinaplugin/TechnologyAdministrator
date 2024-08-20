c4f8cc2b65339cd0b88d8f40b24898175458d983
hina2113 <110330118+hinaplugin@users.noreply.github.com>
2024-08-21 00:49:06 +0900

    roleListCommandの追加

--- deploy.js
+++ deploy.js
@@ -10,7 +10,48 @@ const rolesetCommand = require('./commands/roleset');
  * 環境変数の読み込み
  */
 dotenv.config();
+/**
+ * モジュールの読み込み
+ */
+const { REST, Routes } = require('discord.js');
+const dotenv = require('dotenv');
+const serversetCommand = require('./commands/serverset');
+const rolesetCommand = require('./commands/roleset');
+const rolelistCommand = require('./commands/rolelist');
 
+/**
+ * 環境変数の読み込み
+ */
+dotenv.config();
+
+/**
+ * 登録するコマンド
+ */
+const commands = [
+    serversetCommand.data.toJSON(),
+    rolesetCommand.data.toJSON(),
+    rolelistCommand.data.toJSON(),
+];
+
+/**
+ * Restの設定
+ */
+const rest = new REST({ version: '10'}).setToken(process.env.DISCORD_TOKEN);
+
+/**
+ * コマンド登録
+ */
+(async () => {
+    try{
+        await rest.put(
+            Routes.applicationGuildCommands(process.env.APPLICATION_ID, process.env.GUILD_ID),
+            { body: commands },
+        );
+        console.log('コマンドの登録が完了しました(・ω・)');
+    }catch(error){
+        console.error('コマンドの登録に失敗しました．', error);
+    }
+})();
 /**
  * 登録するコマンド
  */
