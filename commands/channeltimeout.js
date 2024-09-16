f3135bb9f473a3bba055f07432475b0f9340290e
hina2113 <110330118+hinaplugin@users.noreply.github.com>
2024-09-17 01:50:57 +0900

    Update channeltimeout.js

--- commands/channeltimeout.js
+++ commands/channeltimeout.js
@@ -19,6 +19,7 @@ module.exports = {
                 .setName('channeltimeout')
                 .setDescription('チャンネル別のタイムアウト')
                 .addMentionableOption(option => option.setName('target').setDescription('タイムアウトのターゲット').setRequired(true))
+                .addChannelOption(option => option.setName('channel').setDescription('タイムアウトするチャンネル').setRequired(true))
                 .addBooleanOption(option => option.setName('set').setDescription('設定または解除').setRequired(true)),
     execute: async function(interaction) {
 
@@ -28,6 +29,35 @@ module.exports = {
 
         const target = options.getMentionable('target');
 
-        await interaction.reply(`メンション: ${target}`);
+        const channel = options.getChannel('channel');
+
+        const set = options.getBoolean('set');
+
+        const tomlContent = fs.readFileSync(filePath, 'utf-8');
+
+        const config = toml.parse(tomlContent);
+
+        if (!target.user) {
+            await interaction.reply({ content: 'ユーザーをメンションしてください．', ephemeral: true });
+            return;
+        }
+
+        if (config.timeout && Array.isArray(config.timeout)) {
+            if (set) {
+                const isSet = config.timeout.find(member => member.id === target.user.id);
+                if (isSet) {
+                    await interaction.reply({ content: `${target}は既に${channel}でタイムアウトされています．`, ephemeral: true });
+                    return;
+                }
+
+                await interaction.reply(`メンション: ${target}, チャンネル: ${channel}`);
+            }else {
+                const isSet = config.timeout.find(member => member.id === target.user.id);
+                if (!isSet) {
+                    await interaction.reply({ content: `${target}は${channel}でタイムアウトされていません．`, ephemeral: true });
+                    return;
+                }
+            }
+        }
     }
 }
