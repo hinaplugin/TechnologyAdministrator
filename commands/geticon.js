176fd32f78f7fbc1dae3e0a522a201a3ff66729f
hina2113 <110330118+hinaplugin@users.noreply.github.com>
2024-10-09 17:17:51 +0900

    Add files via upload

---
+++ commands/geticon.js
@@ -0,0 +1,40 @@
+/**
+ * モジュールの読み込み
+ */
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const fs = require('fs');
+const toml = require('@iarna/toml');
+const path = require('path');
+
+/**
+ * tomlファイルのパス
+ */
+const filePath = path.resolve(__dirname, "../../admin.toml");
+
+/**
+ * モジュール作成
+ */
+module.exports = {
+    data: new SlashCommandBuilder()
+                .setName('geticon')
+                .setDescription('ユーザーのアイコンを取得')
+                .addMentionableOption(option => option.setName('target').setDescription('取得したいユーザー').setRequired(true))
+                .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild),
+    execute: async function(interaction) {
+
+        await interaction.deferReply();
+
+        const { options } = interaction;
+
+        const targetMember = options.getMentionable('target');
+
+        const target = await interaction.guild.members.fetch(targetMember.user.id);
+
+        if (!target) {
+            await interaction.editReply({ content: `${target}を取得できませんでした．`, ephemeral: true });
+            return;
+        }
+
+        await interaction.editReply({ content: `〇 ${target.displayName}\n${target.displayAvatarURL()}`, ephemeral: true });
+    }
+}
