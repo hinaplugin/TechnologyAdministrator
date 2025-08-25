eb02401113bd3e867adeddcf8a6ee25ec4528104
hina2113 <hina2113@hinaplugin.com>
2025-08-25 23:06:01 +0900

    セットリストの表示を修正

--- commands/set-list.js
+++ commands/set-list.js
@@ -74,7 +74,7 @@ async function getSetList() {
     let i = 1;
     let description = "";
     await list.forEach(title => {
-        description += i + title;
+        description += i + ". " + title;
         if (i == now) {
             description += " ← Now";
         }
